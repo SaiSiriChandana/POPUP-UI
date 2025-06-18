cfbb49545f8de13944cd460e4dc76039972d5632
Kurella Sai Siri Chandana <152707944+SaiSiriChandana@users.noreply.github.com>
2025-06-18 21:35:35 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,7 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-   apiKey: "AIzaSyB3xHSECSWVn-eBBHzFX8SQCQNomYFOAeU",
-  authDomain: "mood-based-recommendatio-2c7a9.firebaseapp.com",
-  projectId: "mood-based-recommendatio-2c7a9",
-  storageBucket: "mood-based-recommendatio-2c7a9.firebasestorage.app",
-  messagingSenderId: "319741143296",
-  appId: "1:319741143296:web:962498ff41379d436e2be5",
-  measurementId: "G-GHXHXTBFMF"
+
 };
 
 const app = initializeApp(firebaseConfig);
