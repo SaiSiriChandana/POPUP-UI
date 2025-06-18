c44018a9ec2083dc4163da62685bb578b2442cfe
Kurella Sai Siri Chandana <152707944+SaiSiriChandana@users.noreply.github.com>
2025-06-18 21:34:59 +0530

    Update ActivitySelection.js

--- src/ActivitySelection.js
+++ src/ActivitySelection.js
@@ -6,13 +6,7 @@ import { getFirestore, doc, setDoc } from 'firebase/firestore';
 
 const firebaseConfig = {
   // 🔐 Your Firebase config here
-  apiKey: "AIzaSyB3xHSECSWVn-eBBHzFX8SQCQNomYFOAeU",
-  authDomain: "mood-based-recommendatio-2c7a9.firebaseapp.com",
-  projectId: "mood-based-recommendatio-2c7a9",
-  storageBucket: "mood-based-recommendatio-2c7a9.firebasestorage.app",
-  messagingSenderId: "319741143296",
-  appId: "1:319741143296:web:962498ff41379d436e2be5",
-  measurementId: "G-GHXHXTBFMF"
+ 
 
 };
 
