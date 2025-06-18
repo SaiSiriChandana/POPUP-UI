2ece4a0589701d19b080a3c03a30cc83e29e963a
Kurella Sai Siri Chandana <152707944+SaiSiriChandana@users.noreply.github.com>
2025-06-18 21:35:21 +0530

    Update FeedbackPopup.js

--- src/FeedbackPopup.js
+++ src/FeedbackPopup.js
@@ -14,13 +14,7 @@ import './FeedbackPopup.css';
 const userImage = 'https://cdn-icons-png.flaticon.com/512/847/847969.png';
 
 const firebaseConfig = {
-   apiKey: "AIzaSyB3xHSECSWVn-eBBHzFX8SQCQNomYFOAeU",
-  authDomain: "mood-based-recommendatio-2c7a9.firebaseapp.com",
-  projectId: "mood-based-recommendatio-2c7a9",
-  storageBucket: "mood-based-recommendatio-2c7a9.firebasestorage.app",
-  messagingSenderId: "319741143296",
-  appId: "1:319741143296:web:962498ff41379d436e2be5",
-  measurementId: "G-GHXHXTBFMF"
+ 
 };
 
 const app = initializeApp(firebaseConfig);
